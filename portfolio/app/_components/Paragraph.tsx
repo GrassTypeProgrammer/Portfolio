716688463d83841192776f6183b72a59df2d2e48
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-05-29 08:35:24 +0100

    Updated the paragraph component to have an optional caption style

--- portfolio/app/_components/Paragraph.tsx
+++ portfolio/app/_components/Paragraph.tsx
@@ -1,12 +1,20 @@
+import classNames from 'classnames'
 import React from 'react'
 
 interface Props {
-    text: string;
+    text: string,
+    isCaption?: string,
+    modifiers?: string,
 }
 
 const Paragraph = (props: Props) => {
     return (
-        <p className="whitespace-pre-line text-lg">
+        <p className={classNames(
+            "whitespace-pre-line text-lg",
+            { 'text-center italic': props.isCaption },
+            props.modifiers
+        )}
+        >
             {props.text}
         </p>
     )
